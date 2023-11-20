2d479f7e56c97a9f602a7d4d7ea20580e35edeaf
leedoyeon849 <do83430208@gmail.com>
2023-11-20 16:41:04 +0900

    feat: gray_outline 컬러 추가

--- app/src/main/java/com/example/fluffy_aos/ui/theme/Colors.kt
+++ app/src/main/java/com/example/fluffy_aos/ui/theme/Colors.kt
@@ -6,6 +6,8 @@ val main_orange = Color(0xFFFD8935)
 val main_orange_light = Color(0x26FD8935)
 val main_yellow = Color(0xFFF5A70E)
 val main_yellow_light = Color(0x26FDB935)
+
 val gray_background = Color(0xFFF8F6F5)
 val gray_background_deep = Color(0xFFF2F0EE)
 val gray_text_light = Color(0xFF767676)
+val gray_outline = Color(0xFFF5F5F5)
