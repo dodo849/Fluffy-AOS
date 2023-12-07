8f333b0e9cba3df95b2b541d61db3e58ca175745
leedoyeon849 <do83430208@gmail.com>
2023-12-07 19:39:38 +0900

    feat: survey 필드 포커싱 추가

--- app/src/main/java/com/example/fluffy_aos/ui/common/survey/resuable/NumericField.kt
+++ app/src/main/java/com/example/fluffy_aos/ui/common/survey/resuable/NumericField.kt
@@ -9,7 +9,11 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
@@ -20,6 +24,8 @@ fun NumericField(
     text: String,
     onValueChange: (String) -> Unit,
 ) {
+    val focusRequester = remember { FocusRequester() }
+
     TextField(
         text,
         onValueChange = {
@@ -41,5 +47,12 @@ fun NumericField(
             .fillMaxWidth()
             .height(65.dp)
             .border(0.dp, Color.Transparent)
+            .focusRequester(focusRequester)
     )
+    // Trigger focus when the composable is first laid out
+    DisposableEffect(Unit) {
+        focusRequester.requestFocus()
+        onDispose { }
+    }
+
 }
