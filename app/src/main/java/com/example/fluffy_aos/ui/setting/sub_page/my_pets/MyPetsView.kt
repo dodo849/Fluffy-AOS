64896b359374155e8e3358ce7e6fa587b0b4d876
leedoyeon849 <do83430208@gmail.com>
2023-12-08 09:29:42 +0900

    feat: 설정 > 내 반려동물 화면 추가

---
+++ app/src/main/java/com/example/fluffy_aos/ui/setting/sub_page/my_pets/MyPetsView.kt
@@ -0,0 +1,16 @@
+package com.example.fluffy_aos.ui.setting.sub_page.my_pets
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import com.example.fluffy_aos.ui.setting.sub_page.my_pets.view_model.MyPetsViewModel
+
+@Composable
+fun MyPetsView(
+    viewModel: MyPetsViewModel
+) {
+    Column() {
+        Text("vidfdjfsdf")
+        Text("sdfsdfsdf")
+    }
+}
---
+++ app/src/main/java/com/example/fluffy_aos/ui/setting/sub_page/my_pets/view_model/MyPetsViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.fluffy_aos.ui.setting.sub_page.my_pets.view_model
+
+import com.example.fluffy_aos.db.PetRepository
+
+class MyPetsViewModel(
+    PetRepository: PetRepository
+) {
+}
